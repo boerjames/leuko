b0ff98adf6d91e79954d7eb95b65ae33e7ed6efd
James Boer <boer.james@gmail.com>
2016-04-23 13:36:37 -0500

    testing db build ds

--- src/GenerateDataSource.lua
+++ src/GenerateDataSource.lua
@@ -6,25 +6,15 @@ require 'torchx'
 --[[command line arguments]]
 local cmd = torch.CmdLine()
 cmd:text()
-
---[[database connection settings]]
 cmd:option('--host',                'facetag-db',   'the host connect to')
 cmd:option('--dbname',              'facetag',      'the db to use')
 cmd:option('--user',                'facetag',      'the user to use')
 cmd:option('--password',            '',             'the password for the user, do not fill this in, use cmd line')
-
---[[data parameters]]
 cmd:option('--validPercentage',     0.15,           'percentage of data to use for validation')
 cmd:option('--testPercentage',      0.15,           'perctage of date to use for testing')
 cmd:option('--dataSize',            '{3,40,40}',    'the shape of the input data')
-
---[[preprocessing]]
-cmd:option('--lcn', false, 'apply Yann LeCun Local Contrast Normalization')
-
---[[verbosity]]
-cmd:option('--silent', false, 'dont print anything to stdout')
-
---[[parse these options]]
+cmd:option('--lcn',                 false,          'apply Yann LeCun Local Contrast Normalization')
+cmd:option('--silent',              false,          'dont print anything to stdout')
 cmd:text()
 local opt = cmd:parse(arg or {})
 if not opt.silent then
@@ -38,14 +28,12 @@ local env = require('luasql.postgres'):postgres()
 local conn = env:connect("host=" .. opt.host .. " user=" .. opt.user .. " dbname=" .. opt.dbname .. " password=" .. opt.password)
 
 --[[generate the datasource]]--
-
 -- loop through the images
---local eye_cursor = conn:execute("select eye_tag.id, eye_tag.label, eye_tag.left, eye_tag.top, eye_tag.width, eye_tag.height, eye_tag.image_id from eye_tag")
 local image_cursor = conn:execute("select image.id from image")
 local num_image_rows = image_cursor:numrows()
 local image_res = {}
 
-for i = 1, num_image_rows do
+for i = 1, 100 do
     image_cursor:fetch(image_res, "a")
     print('image ' .. image_res["id"])
 
@@ -53,12 +41,87 @@ for i = 1, num_image_rows do
     local num_eye_tags = eye_cursor:numrows()
     local eye_tags, eye_tags_tmp = {}, {}
 
-    for j = 1, num_eye_rows do
+    for j = 1, num_eye_tags do
         eye_cursor:fetch(eye_tags_tmp, "a")
         table.insert(eye_tags, eye_tags_tmp)
     end
 
+    for j = 1, #eye_tags
+
+    collectgarbage()
     print(image_res["id"])
     print(eye_tags)
     print()
 end
+
+local function pixelset(crop, image_width, image_height)
+    local left, top, width, height = unpack(crop)
+    local set = {}
+
+    for j = top, top + height - 1 do
+        for i = left, left + width - 1 do
+            local pixelnum = (j - 1) * image_width + i
+            set[pixelnum] = true
+        end
+    end
+    return set
+end
+
+local function setsimilarity(s1, s2)
+    local u, i = {}, {}
+    local s1len, s2len, ulen, ilen = 0, 0, 0, 0
+
+    -- build union set
+    for k,v in pairs(s1) do
+        if v then
+            u[k] = v
+            s1len = s1len + 1
+        end
+    end
+
+    for k,v in pairs(s2) do
+        if v then
+            u[k] = v
+            s2len = s2len + 1
+        end
+    end
+
+    -- build intersection set
+    for k1,v1 in pairs(s1) do
+        local v2 = s2[k1]
+        if v1 and v2 then
+            i[k1] = v1
+        end
+    end
+
+    for k,v in pairs(u) do
+        ulen = ulen + 1
+    end
+
+    for k,v in pairs(i) do
+        ilen = ilen + 1
+    end
+
+    local overlap = ilen / ulen
+    if s1len > s2len then
+        return overlap, 1
+    else
+        return overlap, 2
+    end
+
+end
+
+--local image_width, image_height = 1024, 1024
+--local crop1, crop2, crop3, crop4 = {50,50,100,100}, {49,49,100,100}, {100,100,200,200}, {150,150,100,100}
+--local pixelsets = {}
+--table.insert(pixelsets, pixelset(crop1, image_width, image_height))
+--table.insert(pixelsets, pixelset(crop2, image_width, image_height))
+--table.insert(pixelsets, pixelset(crop3, image_width, image_height))
+--table.insert(pixelsets, pixelset(crop4, image_width, image_height))
+
+--for i = 1, #pixelsets do
+--    for j = i + 1, #pixelsets do
+--        local sim, iorj = setsimilarity(pixelsets[i], pixelsets[j])
+--        print(i, j, sim, iorj)
+--    end
+--end
