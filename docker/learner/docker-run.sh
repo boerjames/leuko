755693f40c0020359317868430f100b094df3b3b
root <root@facetag2.ecs.baylor.edu>
2016-04-27 20:41:11 -0500

    docker finishing up

--- docker/learner/docker-run.sh
+++ docker/learner/docker-run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run -it -v /home/grad/boer/deep:/root/deep --link hypero-db:hypero-db --device /dev/nvidiactl --device /dev/nvidia-uvm --device /dev/nvidia0 --device /dev/nvidia1 --name leuko-learner learner /bin/bash
+docker run -it -v /home/grad/boer/shared:/root/shared --link hypero-db:hypero-db --link facetag-db:facetag-db --device /dev/nvidiactl --device /dev/nvidia-uvm --device /dev/nvidia0 --device /dev/nvidia1 --name learner learner /bin/bash
