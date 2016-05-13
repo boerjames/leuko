3acc01b85cc79bae94334f5f4235c8cd758d4872
James Boer <boer.james@gmail.com>
2016-05-12 20:43:57 -0500

    learner docker parameter

--- docker/learner/docker-run.sh
+++ docker/learner/docker-run.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
-docker run -it -v /home/grad/boer/shared:/root/shared --link hypero-db:hypero-db --link facetag-db:facetag-db --device /dev/nvidiactl --device /dev/nvidia-uvm --device /dev/nvidia0 --device /dev/nvidia1 --name learner learner /bin/bash
+NAME=$1
+
+docker run -it -v /home/grad/boer/shared:/root/shared --link hypero-db:hypero-db --link facetag-db:facetag-db --device /dev/nvidiactl --device /dev/nvidia-uvm --device /dev/nvidia0 --device /dev/nvidia1 --name $NAME learner /bin/bash
